3c9c05c3f0739519d9c650312c30b00295b65f2a
Jung Seung Min <jsm1592@gmail.com>
2024-09-17 18:01:00 +0900

    memoryDB

---
+++ src/main/java/com/example/demo/book/db/service/BookService.java
@@ -0,0 +1,30 @@
+package com.example.demo.book.db.service;
+
+import com.example.demo.book.db.entity.BookEntity;
+import com.example.demo.book.db.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    public final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // create, update
+    public BookEntity create(BookEntity book) {
+        return bookRepository.save(book);
+    }
+    // all
+    public List<BookEntity> findAll() {
+        return bookRepository.findAll();
+    }
+
+    // delete
+
+    // find one
+}
